06a72fc8307b819f82120267d55b770102a58c78
schenker332 <schenker.schenk@gmail.com>
2024-06-07 14:52:04 +0200

    jjaaf

--- finder/lib/main.dart
+++ finder/lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Container(
+          width: 200,
+          height:100,
+          decoration: BoxDecoration(
+            color: Colors.blue
+          ),
+        ),
+      )
+    ),
+  ));
 }
