21a471ef9e6614dae943b38b4c50a815ccc0e008
merdanc <merdanc@tu-berlin.de>
2024-06-14 15:36:47 +0200

    salwas version working

--- finder/lib/main.dart
+++ finder/lib/main.dart
@@ -1,39 +1,49 @@
 import 'package:flutter/material.dart';
-import 'food_card.dart';
-import 'fooditems.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:foodfinder_app/HomePage.dart';
 
 void main() {
-  runApp(FoodApp());
+  runApp(MyFirstApp());
 }
 
-class FoodApp extends StatelessWidget {
+class MyFirstApp extends StatelessWidget {
+  const MyFirstApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Food App'),
+      title: "FoodFinder",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          primary: Colors.white,
+          onPrimary: const Color(0xFFF5DC51),
+          secondary: const Color(0xFFED736B),
+          onSecondary: const Color(0xFF71BD71),
         ),
-        body: ListView.builder(
-          padding: const EdgeInsets.all(16.0),
-          itemCount: foodItems.length,
-          itemBuilder: (context, index) {
-            final foodItem = foodItems[index];
-            return Container(
-              margin: const EdgeInsets.only(bottom: 16.0),
-              height: 300, // Setze die Höhe der Karte auf 300 Pixel
-              child: FoodCard(
-                title: foodItem.title,
-                imagePath: foodItem.imagePath,
-                isVegan: foodItem.isVegan,
-                price: foodItem.price,
-                description: foodItem.description,
-                availability: foodItem.availability,
-              ),
-            );
-          },
+        scaffoldBackgroundColor: const Color(0xFFFFFBF9),
+        appBarTheme: AppBarTheme(
+          backgroundColor: const Color(0xFFFFFBF9),
+        ),
+        primaryColor: const Color(0xFFFBF9),
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        textTheme: TextTheme(
+          titleLarge: GoogleFonts.afacad(
+            fontSize: 32,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+          bodyMedium: GoogleFonts.afacad(
+            color: Colors.black,
+            fontSize: 10,
+            letterSpacing: 0,
+          ),
         ),
       ),
+      home: HomePage(),
     );
   }
 }
