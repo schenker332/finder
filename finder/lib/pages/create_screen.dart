b249c9917603e83f9d1cd8a31f57f175ef1ade0c
seissa24 <salwa10045@gmail.com>
2024-06-14 21:31:53 +0200

    Second Commit!

---
+++ finder/lib/pages/create_screen.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class CreateScreen extends StatelessWidget {
+  const CreateScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purpleAccent,
+    );
+  }
+}
---
+++ finder/lib/pages/plan_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PlanScreen extends StatelessWidget {
+  const PlanScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+    );
+  }
+}
