5c78a552696227d64cf26709501a1af4c7eff3ee
Ren-ram <renram@gmx.de>
2024-07-18 09:35:10 +0200

    ich muss Commiten, damit ic die neue Main laden kann. Diese Version ist egal

--- finder/lib/pages/favorite_screen.dart
+++ finder/lib/pages/favorite_screen.dart
@@ -1,44 +1,123 @@
-
 import 'package:flutter/material.dart';
+import '../Data/foodcard.dart';
+import '../Widgets/foodcard_design.dart';
+import '../Data/foodcard_storage.dart';
 
-class FavoriteScreen extends StatelessWidget {
+class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
+
+  @override
+  _FavoriteScreenState createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  final TextEditingController searchController = TextEditingController();
+  List<Foodcard> likedFoodcards = [];
+  List<Foodcard> filteredFoodcards = [];
+  final FoodcardStorage foodcardStorage = FoodcardStorage();
+
+  @override
+  void initState() {
+    super.initState();
+    loadLikedFoodcards();
+  }
+
+  Future<void> loadLikedFoodcards() async {
+    List<Foodcard> allFoodcards = await foodcardStorage.getFoodcards();
+    List<Foodcard> liked = await foodcardStorage.getLikedRecipes(allFoodcards);
+    setState(() {
+      likedFoodcards = liked;
+      filteredFoodcards = liked;
+    });
+  }
+
+  void filterFoodcards(String query) {
+    setState(() {
+      filteredFoodcards = likedFoodcards.where((foodcard) {
+        return foodcard.title.toLowerCase().contains(query.toLowerCase()) ||
+            foodcard.description.toLowerCase().contains(query.toLowerCase()) ||
+            foodcard.foodart.toLowerCase().contains(query.toLowerCase()) ||
+            foodcard.tags.any((tag) => tag.toLowerCase().contains(query.toLowerCase())) ||
+            foodcard.ingredients.any((ingredient) =>
+                ingredient.any((element) => element.toString().toLowerCase().contains(query.toLowerCase())));
+      }).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "Gespeichert", //Username Nutzername
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(),
-            ),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 36,
-                  height: 36,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(36),
-                    color: Colors.black,
-                  ),
-                  child: Center(
-                    child: Text(
-                      "F",  //Initialien
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        color: Theme.of(context).colorScheme.primary,
-                        fontSize: 27,
-                        fontWeight: FontWeight.bold,
-                      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Gespeichert", //Username Nutzername
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 36,
+                height: 36,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(36),
+                  color: Colors.black,
+                ),
+                child: Center(
+                  child: Text(
+                    "F",  //Initialien
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
                     ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: searchController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  hintText: 'Gespeicherte Rezepte durchsuchen',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  contentPadding: EdgeInsets.symmetric(vertical: 10),
+                ),
+                onChanged: (query) => filterFoodcards(query),
+              ),
+              SizedBox(height: 10),
+              Expanded(
+                child: filteredFoodcards.isNotEmpty
+                    ? ListView.builder(
+                  itemCount: filteredFoodcards.length,
+                  itemBuilder: (context, index) {
+                    return FoodcardDesign(
+                      foodcard: filteredFoodcards[index],
+                    );
+                  },
+                )
+                    : Center(
+                  child: Text(
+                    'Bisher keine Rezepte vorhanden',
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
                 ),
               ),
             ],
           ),
-          //TODO implement Gespeichert LIST
         ),
+      ),
     );
   }
 }
